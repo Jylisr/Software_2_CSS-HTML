ef9a5fb4b5654bee08e24ae564864b875f373d49
Juho <juho.ylisir@gmail.com>
2023-11-08 16:11:38 +0200

    8.11.2023

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-document.write("Bruh")
+
+const button = document.createElement('button')
+button.innerText = 'View our products'
+button.addEventListener('click', openSite, false)
+
+
+function openSite() {
+    alert("test")
+}
